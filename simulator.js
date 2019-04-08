1bd3bbc2ef54ce5d061af6111f0a86d02083f516
Adrián Ortiz Gutiérrez <aortiz@Adrians-MacBook-Pro.local>
2019-04-08 21:44:12 +0200

    formatted

--- simulator.js
+++ simulator.js
@@ -1,29 +1,29 @@
-(function() {
+(function () {
     //include sample data
     var _pathData = [
-        {latitude: 42.347856, longitude: -71.073668},
-        {latitude: 42.347872, longitude: -71.068561, pause: 5000},
-        {latitude: 42.347555, longitude: -71.065986},
-        {latitude: 42.346349, longitude: -71.060793},
-        {latitude: 42.346445, longitude: -71.058433},
-        {latitude: 42.348062, longitude: -71.057832},
-        {latitude: 42.348697, longitude: -71.058648},
-        {latitude: 42.348094, longitude: -71.060128},
-        {latitude: 42.344716, longitude: -71.061416, pause: 15000},
-        {latitude: 42.340529, longitude: -71.063561},
-        {latitude: 42.336928, longitude: -71.065321},
-        {latitude: 42.335469, longitude: -71.065578},
-        {latitude: 42.332630, longitude: -71.063433},
-        {latitude: 42.329473, longitude: -71.060450},
-        {latitude: 42.323794, longitude: -71.055214},
-        {latitude: 42.321398, longitude: -71.054099},
-        {latitude: 42.321366, longitude: -71.051245},
-        {latitude: 42.316876, longitude: -71.048884}
+        { latitude: 42.347856, longitude: -71.073668 },
+        { latitude: 42.347872, longitude: -71.068561, pause: 5000 },
+        { latitude: 42.347555, longitude: -71.065986 },
+        { latitude: 42.346349, longitude: -71.060793 },
+        { latitude: 42.346445, longitude: -71.058433 },
+        { latitude: 42.348062, longitude: -71.057832 },
+        { latitude: 42.348697, longitude: -71.058648 },
+        { latitude: 42.348094, longitude: -71.060128 },
+        { latitude: 42.344716, longitude: -71.061416, pause: 15000 },
+        { latitude: 42.340529, longitude: -71.063561 },
+        { latitude: 42.336928, longitude: -71.065321 },
+        { latitude: 42.335469, longitude: -71.065578 },
+        { latitude: 42.332630, longitude: -71.063433 },
+        { latitude: 42.329473, longitude: -71.060450 },
+        { latitude: 42.323794, longitude: -71.055214 },
+        { latitude: 42.321398, longitude: -71.054099 },
+        { latitude: 42.321366, longitude: -71.051245 },
+        { latitude: 42.316876, longitude: -71.048884 }
     ];
 
     //the geolocation simulator
     var _simulator;
-    
+
     //some stuff to monitor position
     var _timer,
         _timerInterval = 1000, //play with these to see more infrequent results (>= 1000)
@@ -34,7 +34,7 @@
         //makeMap();
 
         //create the simulator, pass pathData array as parameter
-        _simulator = GeolocationSimulator({coords: _pathData, speed: 200});
+        _simulator = GeolocationSimulator({ coords: _pathData, speed: 200 });
 
         //start it up (begin moving)
         _simulator.start();
@@ -67,7 +67,7 @@
     function newPosition(data) {
         _update++;
         $('.output').empty();
-        
+
         output('update ' + _update);
 
         var coords = data.coords;
@@ -75,9 +75,9 @@
         output('lat: ' + coords.latitude.toFixed(7));
         output('lon: ' + coords.longitude.toFixed(7));
 
-        if(map) {
+        if (map) {
             var newLatLng = new google.maps.LatLng(coords.latitude, coords.longitude);
-            map.panTo(newLatLng);    
+            map.panTo(newLatLng);
             marker.setCenter(newLatLng);
         }
     }
@@ -93,31 +93,31 @@
         $('.output').append(p);
     }
 
-/*    function makeMap() {
-        var coords = new google.maps.LatLng(_pathData[0].latitude, _pathData[0].longitude);
-        
-        var myOptions = {
-            zoom: 15,
-            center: coords,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-        };
-
-        map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-        var opts = {
-            strokeColor: '#FF0000',
-            strokeOpacity: 1,
-            strokeWeight: 1,
-            fillColor: '#FF0000',
-            fillOpacity: 0.5,
-            map: map,
-            center: coords,
-            radius: 40
-        };
-        // Add the circle for this city to the map.
-        marker = new google.maps.Circle(opts);
-    }
-      */
+    /*    function makeMap() {
+            var coords = new google.maps.LatLng(_pathData[0].latitude, _pathData[0].longitude);
+            
+            var myOptions = {
+                zoom: 15,
+                center: coords,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+            };
+    
+            map = new google.maps.Map(document.getElementById('map'), myOptions);
+    
+            var opts = {
+                strokeColor: '#FF0000',
+                strokeOpacity: 1,
+                strokeWeight: 1,
+                fillColor: '#FF0000',
+                fillOpacity: 0.5,
+                map: map,
+                center: coords,
+                radius: 40
+            };
+            // Add the circle for this city to the map.
+            marker = new google.maps.Circle(opts);
+        }
+          */
 
     //boot it up
     init();
